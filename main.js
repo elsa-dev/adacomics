2fbe8dfc6b939fa82688378b46c0cfb841229557
Elsa-dev <beadearg@hotmail.com>
2021-02-12 13:48:13 -0300

    conecte con api y arme 1 tarjeta sola

--- main.js
+++ main.js
@@ -5,7 +5,38 @@ fetch("http://gateway.marvel.com/v1/public/comics?ts=1&apikey=a5c341737de7aeb8ed
 })
 .then((info) => {
     console.log(info)
+
+    console.log(info.data.results[4].thumbnail.path)
     console.log(info.data.results[4].title)
+
+    const card = document.querySelector('article');
+
+    card.innerHTML = `
+    <div class="contenedor__tarjetas-imagen">
+            <img  class="imagen" src="${info.data.results[4].thumbnail.path}/portrait_uncanny.${info.data.results[4].thumbnail.extension}" alt="">
+          </div>
+          <h3 class="contenedor__tarjetas-titulo">
+          ${info.data.results[4].title}
+          </h3>
+    
+    `
+
+
+    
+    
+    
+
+    
+
+
+
+
+
+
+
+
+
+
 }
 
 )
