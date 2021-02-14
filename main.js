29f56e7e33fd50b5de749f7e1a277f88d41e9a89
Elsa-dev <beadearg@hotmail.com>
2021-02-14 11:54:31 -0300

    incorpore las 20 tarjetas

--- main.js
+++ main.js
@@ -9,33 +9,22 @@ fetch("http://gateway.marvel.com/v1/public/comics?ts=1&apikey=a5c341737de7aeb8ed
     console.log(info.data.results[4].thumbnail.path)
     console.log(info.data.results[4].title)
 
-    const card = document.querySelector('article');
+    const seccion = document.querySelector('section');
 
-    card.innerHTML = `
-    <div class="contenedor__tarjetas-imagen">
-            <img  class="imagen" src="${info.data.results[4].thumbnail.path}/portrait_uncanny.${info.data.results[4].thumbnail.extension}" alt="">
+    seccion.innerHTML = " ";
+    info.data.results.map( (comic) => {
+        seccion.innerHTML += `
+    <article >
+         <div class="contenedor__tarjetas-imagen">
+            <img  class="imagen" src="${comic.thumbnail.path}/portrait_uncanny.${comic.thumbnail.extension}" alt="">
           </div>
           <h3 class="contenedor__tarjetas-titulo">
-          ${info.data.results[4].title}
-          </h3>
-    
-    `
-
-
-    
-    
-    
-
-    
-
-
-
-
-
-
-
-
-
+          ${comic.title}
+          </h3> 
+      </article> 
+    ` 
+    })
+   
 
 }
 
